c32eadac239fa43b0c485ced6b26513a9684fdfb
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2026-07-05 23:23:56 +0200

    wip

--- verve_backend/queries/grid_raw_data.sql
+++ verve_backend/queries/grid_raw_data.sql
@@ -11,6 +11,6 @@ SELECT
 FROM activities a
 WHERE a.user_id = :user_id
   AND a.start >= CAST(:start_date AS date)
-  AND a.start <= CAST(:end_date AS date)  + interval '1 day'
+  AND a.start < CAST(:end_date AS date)  + interval '1 day'
 GROUP BY CAST(a.start AS date)
 ORDER BY CAST(a.start AS date);
